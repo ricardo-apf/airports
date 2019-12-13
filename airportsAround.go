d534a4f1c92386930e4a7121386251bc3e9aba16
ricardo-apf <a@a>
2019-12-13 11:05:35 +0000

    smal test

--- airportsAround.go
+++ airportsAround.go
@@ -57,7 +57,7 @@ func main() {
 			// validate if the distance is within the search area - some results might exceed since the search area returned is a square
 			if v.Fields.Distance <= area[6] {
 				fmt.Printf("Airport\t%v\nLongitude\t%v\nLatitude\t%v\nDistance\t%v\n\n", v.Fields.Name, v.Fields.Lon, v.Fields.Lat, v.Fields.Distance)
-			} 
+			}
 		}
 	}
 }
@@ -124,4 +124,5 @@ func sortClosest(retAirportList Results) Results {
 		return retAirportList.Rows[i].Fields.Distance < retAirportList.Rows[j].Fields.Distance
 	})
 	return retAirportList
+
 }
