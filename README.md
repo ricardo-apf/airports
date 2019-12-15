cf3c648456e42132ab3c23120be9dae07effd72f
ricardo-apf <ricardo.abp.ferreira@gmail.com>
2019-12-16 00:58:09 +0100

    added support for calling with flags

--- README.md
+++ README.md
@@ -3,9 +3,20 @@ A simple Go program to get the airports in a given area.
 It needs the user coordinates and the distance in degrees to search. 
 
 After installing the Go runime just type:
-$ go run airportsAround.go
+$ go run airportsAround.go and follow the instructions. 
 
-And follow the instructions. 
+Alternatively pass the follow flags when running the command:
+ $ go run airportsAround.go -lon XX -lat YY -d DD
+
+ Where XX is your longitude, YY is your latitude and DD is the search radius (in degrees)
 
 
-oldAirportdb.go is an alternative algorithm that requires max and min coordinates pairs to search for airports. 
+oldAirportdb.go in the old folder is an alternative algorithm that requires max and min coordinates pairs to search for airports. 
+
+
+# Changelog 
+Added support for invoking with flags 
+
+# To do 
+
+Opptimize the flags code, maybe with an new function. 
--- airportsAround.go
+++ airportsAround.go
@@ -10,6 +10,7 @@ import (
 	"net/url"
 	"os"
 	"sort"
+	"strconv"
 )
 
 // Results type to mimic the json from the query
@@ -25,13 +26,39 @@ type Results struct {
 }
 
 func main() {
+
 	area := []float64{0, 0, 0, 0, 0, 0, 0}
-	//Some attempt at ASCII art, because why not !
-	fmt.Println("*********************************\n***** Airports in your area *****\n*********************************")
-	// call the function to get the user inputs
-	area[4] = getInput("Type the value for your Longitude position", "Longitude", 180)
-	area[5] = getInput("Type the value for your Latitude position", "Latitude", 90)
-	area[6] = getInput("Type the value for the search distance (in Degrees)", "Distance", 360)
+
+	if len(os.Args) == 1 { //validates if any arguments were passed to the program on execution
+		//If no arguments were passed, program starts in interactive mode:
+		//Some attempt at ASCII art, because why not !
+		fmt.Println("*********************************\n***** Airports in your area *****\n*********************************")
+		// call the function to get the user inputs
+		area[4] = getInput("Type the value for your Longitude position", "Longitude", 180)
+		area[5] = getInput("Type the value for your Latitude position", "Latitude", 90)
+		area[6] = getInput("Type the value for the search distance (in Degrees)", "Distance", 360)
+
+	} else { // if the prrogram starts with arguments:
+		//validate if th right arguments are passed in the right order
+		//Could've been done with the falgs built in fuction, but I didn't liked the "help" section and it has no built in way to set mandatory flags. It's also on import less
+		if os.Args[1] == "-lon" {
+			area[4] = validate(os.Args[2], 180)
+		} else {
+			help() //if not display healp
+		}
+
+		if os.Args[3] == "-lat" {
+			area[5] = validate(os.Args[4], 90)
+		} else {
+			help()
+		}
+		if os.Args[5] == "-d" {
+			area[6] = validate(os.Args[6], 360)
+		} else {
+			help()
+		}
+
+	}
 
 	// Converts the distance to positive value in case the user typed in a negative
 	area[6] = math.Abs(area[6])
@@ -48,18 +75,9 @@ func main() {
 	//call the function to sort the results from the closest to the farthest of the search area
 	airportList = sortClosest(airportList)
 
-	//validate if any results were found
-	if len(airportList.Rows) == 0 {
-		fmt.Println("No ariports found in the designated area")
-	} else {
-		// print the results
-		for _, v := range airportList.Rows {
-			// validate if the distance is within the search area - some results might exceed since the search area returned is a square
-			if v.Fields.Distance <= area[6] {
-				fmt.Printf("Airport\t%v\nLongitude\t%v\nLatitude\t%v\nDistance\t%v\n\n", v.Fields.Name, v.Fields.Lon, v.Fields.Lat, v.Fields.Distance)
-			}
-		}
-	}
+	// call the function to print the final results
+	resultsPrint(airportList, area[6])
+
 }
 
 //function to grab the user input for the search range and validate it
@@ -81,14 +99,21 @@ func getInput(question string, kind string, limits float64) float64 {
 
 }
 
-//function to get the coordinates for the search range.
-func calcCenter(calcArea []float64) []float64 {
+//function to convert the arguments when running the program to float, check for errors and if the values are within range. If any of them falis, print help.
+func validate(value string, limit float64) float64 {
+	temp, err := strconv.ParseFloat(value, 64)
+	if err != nil || temp > limit || temp < (limit*-1) {
+		help()
+	}
+	return temp
+}
 
+//function to get the coordinates for the search range by adding and subtracting the search distance to each coordinate.
+func calcCenter(calcArea []float64) []float64 {
 	calcArea[1] = (calcArea[4] + calcArea[6])
 	calcArea[0] = (calcArea[4] - calcArea[6])
 	calcArea[3] = (calcArea[5] + calcArea[6])
 	calcArea[2] = (calcArea[5] - calcArea[6])
-
 	return calcArea
 }
 
@@ -100,7 +125,7 @@ func getJSON(getArea []float64) Results {
 	unformatedURL := fmt.Sprintf("lon:[%v TO %v] AND lat:[%v TO %v]", getArea[0], getArea[1], getArea[2], getArea[3])
 	// encode the url
 	formatedURL := url.QueryEscape(unformatedURL)
-	// send the query to the API
+	// send the query to the url
 	response, _ := http.Get("https://mikerhodes.cloudant.com/airportdb/_design/view1/_search/geo?q=" + formatedURL)
 	// get the body from the response
 	body, _ := ioutil.ReadAll(response.Body)
@@ -125,4 +150,28 @@ func sortClosest(retAirportList Results) Results {
 	})
 	return retAirportList
 
+}
+
+//validate if any results were found
+func resultsPrint(airports Results, distance float64) {
+	if len(airports.Rows) == 0 {
+		fmt.Println("No ariports found in the designated area")
+	} else {
+		// print the results
+		for _, v := range airports.Rows {
+			// validate if the distance is within the search area - some results might exceed since the search area returned is a square
+			if v.Fields.Distance <= distance {
+				fmt.Printf("Airport\t%v\nLongitude\t%v\nLatitude\t%v\nDistance\t%v\n\n", v.Fields.Name, v.Fields.Lon, v.Fields.Lat, v.Fields.Distance)
+			}
+		}
+	}
+}
+
+// prints out the help
+func help() {
+	fmt.Println("\nFind airports by distance. Insert a coordinate and search radius to get a list of ariports sorted by distance.\nUsage: ")
+	fmt.Println("\nAirportsAround -lon -lat -d")
+	fmt.Println("Call without arguments for interactive mode")
+	fmt.Println("\nOPTIONS:\n-lon\tLongitude value, between -180 and 180\n-lat\tLatitude value, between -90 and 90\n-d\tSearch distance radius in degrees, values between 0 and 360\n--help\tShow this message")
+	os.Exit(0)
 }
